b8c2a9f2ef7dd7d12afe3d917b1bc5fe80087270
UxAzhangxiaohua <zhangshengyang618@gmail.com>
2024-09-23 03:49:39 +0200

    update23.09

--- pages/index.vue
+++ pages/index.vue
@@ -22,12 +22,26 @@
             <UButton color="gray" icon="i-heroicons-globe-alt" label="Website" to="https://crypto.ethz.ch/teaching/DM24/" target="_blank" />
             <!-- @vue-ignore -->
             <UButton color="gray" icon="i-heroicons-video-camera" label="Video Lectures" to="https://video.ethz.ch/lectures/d-infk/2024/autumn/252-0025-01L.html" target="_blank" />
+            <UButton color="gray" icon="i-heroicons-globe-alt" label="exercise" to="https://dm.crypto.ethz.ch/" target="_blank" />
         </div>
-        <p class="my-2">
-            DiscMath exercises are password protected, because apparently people love them so much they steal them and deal with them on the black market. 
-            For that reason it is non-trivial to get the exercises and display them on this site.
-            Please get them yourself at <UButton variant="link" target="_blank" to="https://dm.crypto.ethz.ch/" label="dm.crypto.ethz.ch" />
-        </p>
+    </div>
+    <div class="flex flex-col px-2">
+        <span class="text-xl font-semibold text-primary-500">Analysis II</span>
+        <div class="flex gap-2 my-4">
+            <UButton color="gray" icon="i-heroicons-globe-alt" label="Website" to="https://moodle-app2.let.ethz.ch/course/view.php?id=23610#section-2" target="_blank" />
+            <!-- @vue-ignore -->
+            <UButton color="gray" icon="i-heroicons-video-camera" label="Video Lectures" to="https://video.ethz.ch/lectures/d-math/2024/autumn/401-0213-16L.html" target="_blank" />
+            <UButton color="gray" icon="i-heroicons-globe-alt" label="exercise" to="https://moodle-app2.let.ethz.ch/course/view.php?id=23610#section-3" target="_blank" />
+        </div>
+    </div>
+    <div class="flex flex-col px-2">
+        <span class="text-xl font-semibold text-primary-500">Theoretische Info</span>
+        <div class="flex gap-2 my-4">
+            <UButton color="gray" icon="i-heroicons-globe-alt" label="Website" to="https://moodle-app2.let.ethz.ch/course/view.php?id=23497" target="_blank" />
+            <!-- @vue-ignore -->
+            <UButton color="gray" icon="i-heroicons-video-camera" label="Video Lectures" to="https://video.ethz.ch/lectures/d-infk/2024/autumn/252-0057-00L.html" target="_blank" />
+            <UButton color="gray" icon="i-heroicons-globe-alt" label="exercise" to="https://moodle-app2.let.ethz.ch/course/view.php?id=23497#section-6" target="_blank" />
+        </div>
     </div>
 </template>
 
