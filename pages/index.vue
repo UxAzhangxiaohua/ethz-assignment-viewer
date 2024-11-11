21f6f6bf4cb026ce6f2d6d1acf40875085e5be78
UxAzhangxiaohua <zhangshengyang618@gmail.com>
2024-11-11 19:21:53 +0100

    update discmath web

--- pages/index.vue
+++ pages/index.vue
@@ -28,6 +28,7 @@
             <UButton color="gray" icon="i-heroicons-video-camera" label="Video Lectures" to="https://video.ethz.ch/lectures/d-infk/2024/autumn/252-0025-01L.html" target="_blank" />
             <UButton color="gray" icon="i-heroicons-globe-alt" label="exercise" to="https://dm.crypto.ethz.ch/" target="_blank" />
             <UButton color="gray" icon="i-heroicons-globe-alt" label="Leon Kolmanic" to="https://leonkolmanic.com/teaching/" target="_blank" />
+            <UButton color="gray" icon="i-heroicons-globe-alt" label="discmath.ch" to="https://discmath.ch/content" target="_blank" />
 
         </div>
     </div>
